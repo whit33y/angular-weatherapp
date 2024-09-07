b554c0abc0a392c313d6b26789aade9a05150b8a
whit33y <troppout@gmail.com>
2024-09-07 18:37:45 +0200

    Forecast form completed

--- src/app/components/forecast-form/forecast-form.component.ts
+++ src/app/components/forecast-form/forecast-form.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forecast-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forecast-form.component.html',
-  styleUrl: './forecast-form.component.css'
+  styleUrl: './forecast-form.component.css',
 })
 export class ForecastFormComponent {
+  formBuilder = inject(FormBuilder);
+  form = this.formBuilder.group({
+    city: [''],
+    number_of_days: [3],
+  });
 
+  submit() {
+    let { city, number_of_days } = this.form.value;
+    if (number_of_days) {
+      number_of_days = +number_of_days;
+    }
+    console.log(city, number_of_days);
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ForecastFormComponent } from '../../components/forecast-form/forecast-form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ForecastFormComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
