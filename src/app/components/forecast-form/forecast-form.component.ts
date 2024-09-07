7e86d5754080762a815d61286d041778a8370001
whit33y <troppout@gmail.com>
2024-09-07 18:10:24 +0200

    Generated forecast form component

---
+++ src/app/components/forecast-form/forecast-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './forecast-form.component.html',
+  styleUrl: './forecast-form.component.css'
+})
+export class ForecastFormComponent {
+
+}
