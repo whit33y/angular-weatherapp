cdcb90cdd4d490b91bcabae8def312cab0163158
Jakub Bryska <jakub.bryska@daw-systems.pl>
2024-09-12 13:25:27 +0200

    Created forecast more information component

---
+++ src/app/components/forecast-components/forecast-components/forecast-more/forecast-more.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast-more',
+  standalone: true,
+  imports: [],
+  templateUrl: './forecast-more.component.html',
+  styleUrl: './forecast-more.component.css'
+})
+export class ForecastMoreComponent {
+
+}
