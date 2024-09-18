0566cef9791d167a14365f7ebc9dd43061712fa4
whit33y <troppout@gmail.com>
2024-09-18 22:22:04 +0200

    Generated new forecast-by-hour component

---
+++ src/app/components/forecast-components/forecast-by-hour/forecast-by-hour.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast-by-hour',
+  standalone: true,
+  imports: [],
+  templateUrl: './forecast-by-hour.component.html',
+  styleUrl: './forecast-by-hour.component.css'
+})
+export class ForecastByHourComponent {
+
+}
